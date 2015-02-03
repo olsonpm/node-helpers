25d095f219c87223cd779fff4e3123cb008fe60e
Phil <philip.olson.m@gmail.com>
2015-02-02 16:39:37 -0800

    fixed tweenlite dependency

--- gsap-defaults.js
+++ gsap-defaults.js
@@ -1,4 +1,8 @@
-var TweenLite = require('TweenLite');
+'use strict';
+
+if (!Power3) {
+    throw new Error("The Power3 gsap easing object doesn't exist in the current scope.  Are you certain TweenLite or TweenMax has been loaded properly?");
+}
 
 module.exports.DURATION = 0.4;
 module.exports.EASE = Power3.easeOut;
