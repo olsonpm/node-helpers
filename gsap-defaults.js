98a92cb270343e5b1ab0f3c778a1ed18f95263b2
Phil <philip.olson.m@gmail.com>
2015-02-02 16:25:25 -0800

    adding gsap-defaults

---
+++ gsap-defaults.js
@@ -0,0 +1,4 @@
+var TweenLite = require('TweenLite');
+
+module.exports.DURATION = 0.4;
+module.exports.EASE = Power3.easeOut;
--- index.js
+++ index.js
@@ -6,3 +6,5 @@ module.exports.psqlWrapper = require('./psql-wrapper');
 module.exports.utils = require('./utils');
 module.exports.Environment = require('./environment');
 module.exports.BunyanStreams = require('./bunyan-streams');
+module.exports.gsapDefaults = require('./gsap-defaults');
+module.exports.BunyanStreams = require('./bunyan-streams');
