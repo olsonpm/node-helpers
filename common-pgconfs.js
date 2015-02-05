ac762c5f2524a55048cb504b6d7544b2b3772f7a
Phil <philip.olson.m@gmail.com>
2015-02-05 01:47:12 -0800

    fixed dependency on hard-coded environment variable names

--- common-pgconfs.js
+++ common-pgconfs.js
@@ -3,8 +3,12 @@
 var PGConf = require('./psql-wrapper').PGConf;
 var e = process.env;
 
-module.exports = {
-    ptq: new PGConf(e.PGUSER, e.PGDATABASE, e.PGPASSWORD, e.PGPORT, '/run/postgresql', true)
-    , ptq_test: new PGConf('ptq_test', 'ptq_test', 'ptq_test', 5432, 'localhost', true)
-    , test: new PGConf(e.PGTESTUSER, e.PGTESTUSER, e.PGTESTPASS, e.PGPORT, 'localhost', true)
-};
+if (e.PGUSER && e.PGDATABASE && e.PGPASSWORD && e.PGPORT) {
+    module.exports.ptq = new PGConf(e.PGUSER, e.PGDATABASE, e.PGPASSWORD, e.PGPORT, '/run/postgresql', true);
+}
+
+if (e.PGTESTUSER && e.PGTESTPASS && e.PGPORT) {
+    module.exports.test = new PGConf(e.PGTESTUSER, e.PGTESTUSER, e.PGTESTPASS, e.PGPORT, 'localhost', true);
+}
+
+module.exports.ptq_test = new PGConf('ptq_test', 'ptq_test', 'ptq_test', 5432, 'localhost', true);
