a6b72e10d032405fe676d4f1fac4ee6c2335ce54
Phil <philip.olson.m@gmail.com>
2015-04-16 15:10:24 -0700

    removing upper case first letter for jshint compliance.  **This is a breaking change

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ module.exports.lazyExtensions = require('./lazy-extensions');
 module.exports.psqlWrapper = require('./psql-wrapper');
 module.exports.utils = require('./utils');
 module.exports.Environment = require('./environment');
-module.exports.BunyanStreams = require('./bunyan-streams');
+module.exports.bunyanStreams = require('./bunyan-streams');
 module.exports.gsapDefaults = require('./gsap-defaults');
 module.exports.config = require('./config');
