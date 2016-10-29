5635fef07391fa8fe490a60bf9baeac25836359b
phil olson <philip.olson@protonmail.ch>
2016-10-29 00:12:23 -0500

    remove app-root-path

--- environment.js
+++ environment.js
@@ -1,14 +1,11 @@
 'use strict';
 
+
 //---------//
 // Imports //
 //---------//
 
-var lazy = require('./lazy-extensions')
-    , Utils = require('./utils')
-    , root = require('app-root-path')
-    , path = require('path')
-    , fs = require('fs');
+var lazy = require('./lazy-extensions');
 
 
 //-------------------//
@@ -20,9 +17,9 @@ Environment.TEST = 'test';
 Environment.PROD = 'prod';
 
 Environment.ENVS = lazy([
-    Environment.DEV
-    , Environment.TEST
-    , Environment.PROD
+  Environment.DEV
+  , Environment.TEST
+  , Environment.PROD
 ]);
 
 Environment.CLIENT_ENV = 'ENV_NODE_ENV';
@@ -33,82 +30,58 @@ Environment.CLIENT_ENV = 'ENV_NODE_ENV';
 //------//
 
 function Environment() {
-    var self = this;
+  var self = this;
 
-    // if require.main.filename is set, then find its directory and require the package.json file 
-    //   to retrieve the environment variable name
-    var tmpDefaultServerEnv;
-    if (require && require.main && require.main.filename) {
-        var rootPath = root.path
-            , rootPkgConf = path.join(root.path, 'package.json')
-            , found = fs.existsSync(rootPkgConf);
-        while (!found) {
-            rootPath = path.dirname(rootPath);
-            rootPkgConf = path.join(rootPath, 'package.json');
-            found = fs.existsSync(rootPkgConf);
-        }
-        if (!found) {
-            throw new Error("Invalid State: No package.json files were found up the directory path starting from '" + root.path);
-        } else {
-            rootPkgConf = require(rootPkgConf);
-        }
+  var my = {
+    HardCoded: null
+    , ServerEnv: null
+    , DefaultServerEnv: 'WEATHER_ACCURACY_NODE_ENV'
+    , AllowDefaultServerEnv: true
+  };
 
-        if (rootPkgConf && rootPkgConf.environment && rootPkgConf.environment.env_var_name) {
-            tmpDefaultServerEnv = rootPkgConf.environment.env_var_name;
+  self.HardCoded = function(hardcoded_) {
+    var res = my.HardCoded;
+    if (arguments.length > 0) {
+      if (hardcoded_ !== null) {
+        if (!self.AllowDefaultServerEnv() && self.ServerEnv() !== null) {
+          throw new Error('Invalid State: When setting ValidateHardCoded, ServerEnv must equal null');
         }
+        Environment.ValidateHardCoded(hardcoded_, true);
+      }
+      my.HardCoded = hardcoded_;
+      res = self;
     }
-    tmpDefaultServerEnv = tmpDefaultServerEnv || null;
+    return res;
+  };
 
-    var my = {
-        HardCoded: null
-        , ServerEnv: null
-        , DefaultServerEnv: tmpDefaultServerEnv
-        , AllowDefaultServerEnv: true
-    };
-
-    self.HardCoded = function(hardcoded_) {
-        var res = my.HardCoded;
-        if (arguments.length > 0) {
-            if (hardcoded_ !== null) {
-                if (!self.AllowDefaultServerEnv() && self.ServerEnv() !== null) {
-                    throw new Error('Invalid State: When setting ValidateHardCoded, ServerEnv must equal null');
-                }
-                Environment.ValidateHardCoded(hardcoded_, true);
-            }
-            my.HardCoded = hardcoded_;
-            res = self;
+  self.ServerEnv = function(serverenv_) {
+    var res = my.ServerEnv;
+    if (arguments.length > 0) {
+      if (serverenv_ !== null) {
+        if (self.HardCoded()) {
+          throw new Error('Invalid State: When setting ValidateServerEnv, HardCoded must equal null');
         }
-        return res;
-    };
+        Environment.ValidateServerEnv(serverenv_, true);
+      }
+      my.ServerEnv = serverenv_;
+      res = self;
+    } else if (self.AllowDefaultServerEnv()) {
+      res = res || my.DefaultServerEnv;
+    }
+    return res;
+  };
 
-    self.ServerEnv = function(serverenv_) {
-        var res = my.ServerEnv;
-        if (arguments.length > 0) {
-            if (serverenv_ !== null) {
-                if (self.HardCoded()) {
-                    throw new Error('Invalid State: When setting ValidateServerEnv, HardCoded must equal null');
-                }
-                Environment.ValidateServerEnv(serverenv_, true);
-            }
-            my.ServerEnv = serverenv_;
-            res = self;
-        } else if (self.AllowDefaultServerEnv()) {
-            res = res || my.DefaultServerEnv;
-        }
-        return res;
-    };
-
-    self.AllowDefaultServerEnv = function(allowdefaultserverenv_) {
-        var res = my.AllowDefaultServerEnv;
-        if (arguments.length > 0) {
-            if (allowdefaultserverenv_ !== null) {
-                Environment.ValidateAllowDefaultServerEnv(allowdefaultserverenv_, true);
-            }
-            my.AllowDefaultServerEnv = allowdefaultserverenv_;
-            res = self;
-        }
-        return res;
-    };
+  self.AllowDefaultServerEnv = function(allowdefaultserverenv_) {
+    var res = my.AllowDefaultServerEnv;
+    if (arguments.length > 0) {
+      if (allowdefaultserverenv_ !== null) {
+        Environment.ValidateAllowDefaultServerEnv(allowdefaultserverenv_, true);
+      }
+      my.AllowDefaultServerEnv = allowdefaultserverenv_;
+      res = self;
+    }
+    return res;
+  };
 }
 
 
@@ -117,70 +90,70 @@ function Environment() {
 //------------//
 
 Environment.ValidateHardCoded = function ValidateHardCoded(input, throwErr) {
-    var msg = '';
+  var msg = '';
 
-    if (typeof input !== 'string') {
-        msg = 'Invalid Argument: <Environment>.ValidateHardCoded requires a typeof string argument';
-    } else {
-        msg = Environment.ValidateEnv(input, throwErr);
-    }
+  if (typeof input !== 'string') {
+    msg = 'Invalid Argument: <Environment>.ValidateHardCoded requires a typeof string argument';
+  } else {
+    msg = Environment.ValidateEnv(input, throwErr);
+  }
 
-    if (throwErr && msg) {
-        throw new Error(msg);
-    }
+  if (throwErr && msg) {
+    throw new Error(msg);
+  }
 
-    return msg;
+  return msg;
 };
 
 Environment.ValidateServerEnv = function ValidateServerEnv(input, throwErr) {
-    var msg = '';
+  var msg = '';
 
-    if (typeof input !== 'string') {
-        msg = 'Invalid Argument: <Environment>.ValidateServerEnv requires a typeof string';
-    } else if (!process
-        || !process.env
-        || typeof process.env[input] === 'undefined'
-        || Environment.ValidateEnv(process.env[input])) {
+  if (typeof input !== 'string') {
+    msg = 'Invalid Argument: <Environment>.ValidateServerEnv requires a typeof string';
+  } else if (!process
+    || !process.env
+    || typeof process.env[input] === 'undefined'
+    || Environment.ValidateEnv(process.env[input])) {
 
-        msg = "Invalid Argument: process.env." + input + " is undefined.  <Environment>.ValidateServerEnv requires an existing environment variable name";
-    }
+    msg = "Invalid Argument: process.env." + input + " is undefined.  <Environment>.ValidateServerEnv requires an existing environment variable name";
+  }
 
-    if (throwErr && msg) {
-        throw new Error(msg);
-    }
+  if (throwErr && msg) {
+    throw new Error(msg);
+  }
 
-    return msg;
+  return msg;
 };
 
 Environment.ValidateAllowDefaultServerEnv = function ValidateAllowDefaultServerEnv(input, throwErr) {
-    var msg = '';
-    if (typeof input !== 'boolean') {
-        msg = 'Invalid Argument: <Environment>.ValidateAllowDefaultServerEnv requires a typeof boolean argument';
-    }
+  var msg = '';
+  if (typeof input !== 'boolean') {
+    msg = 'Invalid Argument: <Environment>.ValidateAllowDefaultServerEnv requires a typeof boolean argument';
+  }
 
-    if (throwErr && msg) {
-        throw new Error(msg);
-    }
+  if (throwErr && msg) {
+    throw new Error(msg);
+  }
 
-    return msg;
+  return msg;
 };
 
 Environment.ValidateEnv = function ValidateEnv(env, throwErr) {
-    var msg = "";
+  var msg = "";
 
-    if (typeof env !== 'string') {
-        throw new Error("Invalid Argument: Environment.ValidateEnv only validates environment strings");
-    }
+  if (typeof env !== 'string') {
+    throw new Error("Invalid Argument: Environment.ValidateEnv only validates environment strings");
+  }
 
-    if (!Environment.ENVS.has(env.toLowerCase())) {
-        msg = "Invalid Argument: environment '" + env + "' doesn't match one of the following " + Environment.ENVS.toArray();
-    }
+  if (!Environment.ENVS.has(env.toLowerCase())) {
+    msg = "Invalid Argument: environment '" + env + "' doesn't match one of the following " + Environment.ENVS.toArray();
+  }
 
-    if (throwErr && msg) {
-        throw new Error(msg);
-    }
+  if (throwErr && msg) {
+    throw new Error(msg);
+  }
 
-    return msg;
+  return msg;
 };
 
 
@@ -189,37 +162,37 @@ Environment.ValidateEnv = function ValidateEnv(env, throwErr) {
 //-----------------------//
 
 Environment.prototype.getCurrentEnvironment = function getCurrentEnvironment() {
-    this._validateState();
+  this._validateState();
 
-    var res;
+  var res;
 
-    if (this.HardCoded()) {
-        res = this.HardCoded();
-    } else if (process
-        && process.env
-        && process.env[this.ServerEnv()]
-        && Environment.ENVS.contains(process.env[this.ServerEnv()].toLowerCase())) {
-        res = process.env[this.ServerEnv()];
-    } else if (Environment.ENVS.contains(Environment.CLIENT_ENV)) {
-        res = Environment.CLIENT_ENV;
-    }
+  if (this.HardCoded()) {
+    res = this.HardCoded();
+  } else if (process
+    && process.env
+    && process.env[this.ServerEnv()]
+    && Environment.ENVS.contains(process.env[this.ServerEnv()].toLowerCase())) {
+    res = process.env[this.ServerEnv()];
+  } else if (Environment.ENVS.contains(Environment.CLIENT_ENV)) {
+    res = Environment.CLIENT_ENV;
+  }
 
-    if (!res || !Environment.ENVS.contains(res.toLowerCase())) {
-        throw new Error('Invalid State: No environment was found.  This could be due to relying on a non-existent default ServerEnv.');
-    }
+  if (!res || !Environment.ENVS.contains(res.toLowerCase())) {
+    throw new Error('Invalid State: No environment was found.  This could be due to relying on a non-existent default ServerEnv.');
+  }
 
-    return res;
+  return res;
 };
 Environment.prototype.curEnv = Environment.prototype.getCurrentEnvironment;
 
 Environment.prototype.isDev = function isDev() {
-    return this.getCurrentEnvironment() === Environment.DEV;
+  return this.getCurrentEnvironment() === Environment.DEV;
 };
 Environment.prototype.isTest = function isTest() {
-    return this.getCurrentEnvironment() === Environment.TEST;
+  return this.getCurrentEnvironment() === Environment.TEST;
 };
 Environment.prototype.isProd = function isProd() {
-    return this.getCurrentEnvironment() === Environment.PROD;
+  return this.getCurrentEnvironment() === Environment.PROD;
 };
 
 
@@ -228,14 +201,14 @@ Environment.prototype.isProd = function isProd() {
 //-------------------------------//
 
 Environment.prototype._validateState = function _validateState() {
-    // invalid if default server environment isn't allowed, and neither HardCoded nor ServerEnv are set
-    if (this.HardCoded() === null
-        && this.ServerEnv() === null
-        && !Environment.ENVS.contains(Environment.CLIENT_ENV)) {
+  // invalid if default server environment isn't allowed, and neither HardCoded nor ServerEnv are set
+  if (this.HardCoded() === null
+    && this.ServerEnv() === null
+    && !Environment.ENVS.contains(Environment.CLIENT_ENV)) {
 
-        throw new Error("Invalid State: None of the following cases were met\n1) A hard coded environment string is passed.\n2) Called server-side with an environment variable"
-            + "(specified by ServerEnv()) declared.\n3) Called client-side with the server replacing ENV_" + "NODE_ENV with the proper node environment string");
-    }
+    throw new Error("Invalid State: None of the following cases were met\n1) A hard coded environment string is passed.\n2) Called server-side with an environment variable"
+      + "(specified by ServerEnv()) declared.\n3) Called client-side with the server replacing ENV_" + "NODE_ENV with the proper node environment string");
+  }
 };
 
 
--- log-provider.js
+++ log-provider.js
@@ -1,16 +1,15 @@
 'use strict';
 
+
 //---------//
 // Imports //
 //---------//
 
 var bunyan = require('bunyan')
-    , bunyanStreams = require('./bunyan-streams')
-    , Utils = require('./utils')
-    , Environment = require('./environment')
-    , root = require('app-root-path')
-    , path = require('path')
-    , fs = require('fs');
+  , bunyanStreams = require('./bunyan-streams')
+  , Environment = require('./environment')
+  , Utils = require('./utils')
+  ;
 
 
 //------//
@@ -18,88 +17,68 @@ var bunyan = require('bunyan')
 //------//
 
 function LogProvider() {
-    var self = this;
+  var self = this;
 
-    var tmpDefaultAppName;
-    if (require && require.main && require.main.filename) {
-        var rootPath = root.path
-            , rootPkgConf = path.join(root.path, 'package.json')
-            , found = fs.existsSync(rootPkgConf);
-        while (!found) {
-            rootPath = path.dirname(rootPath);
-            rootPkgConf = path.join(rootPath, 'package.json');
-            found = fs.existsSync(rootPkgConf);
-        }
-        if (!found) {
-            throw new Error("Invalid State: No package.json files were found up the directory path starting from '" + root.path);
-        } else {
-            rootPkgConf = require(rootPkgConf);
-        }
+  var my = {
+    EnvInst: null
+    , AppName: null
+    , AllowDefaultEnvInst: true
+    , AllowDefaultAppName: true
+    , DefaultEnvInst: new Environment()
+    , DefaultAppName: 'weather-accuracy'
+  };
 
-        tmpDefaultAppName = rootPkgConf.name;
+  self.EnvInst = function(envinst_) {
+    var res = my.EnvInst;
+    if (arguments.length > 0) {
+      if (envinst_ !== null) {
+        LogProvider.ValidateEnvInst(envinst_, true);
+      }
+      my.EnvInst = envinst_;
+      res = self;
+    } else if (self.AllowDefaultEnvInst()) {
+      res = res || my.DefaultEnvInst;
     }
-    tmpDefaultAppName = tmpDefaultAppName || null;
+    return res;
+  };
 
-    var my = {
-        EnvInst: null
-        , AppName: null
-        , AllowDefaultEnvInst: true
-        , AllowDefaultAppName: true
-        , DefaultEnvInst: new Environment()
-        , DefaultAppName: tmpDefaultAppName
-    };
+  self.AppName = function(appname_) {
+    var res = my.AppName;
+    if (arguments.length > 0) {
+      if (appname_ !== null) {
+        LogProvider.ValidateAppName(appname_, true);
+      }
+      my.AppName = appname_;
+      res = self;
+    } else if (self.AllowDefaultEnvInst()) {
+      res = res || my.DefaultAppName;
+    }
+    return res;
+  };
 
-    self.EnvInst = function(envinst_) {
-        var res = my.EnvInst;
-        if (arguments.length > 0) {
-            if (envinst_ !== null) {
-                LogProvider.ValidateEnvInst(envinst_, true);
-            }
-            my.EnvInst = envinst_;
-            res = self;
-        } else if (self.AllowDefaultEnvInst()) {
-            res = res || my.DefaultEnvInst;
-        }
-        return res;
-    };
+  self.AllowDefaultEnvInst = function(allowdefaultenvinst_) {
+    var res = my.AllowDefaultEnvInst;
+    if (arguments.length > 0) {
+      if (allowdefaultenvinst_ !== null) {
+        LogProvider.ValidateAllowDefaultEnvInst(allowdefaultenvinst_, true);
+      }
+      my.AllowDefaultEnvInst = allowdefaultenvinst_;
+      res = self;
+    }
+    return res;
+  };
 
-    self.AppName = function(appname_) {
-        var res = my.AppName;
-        if (arguments.length > 0) {
-            if (appname_ !== null) {
-                LogProvider.ValidateAppName(appname_, true);
-            }
-            my.AppName = appname_;
-            res = self;
-        } else if (self.AllowDefaultEnvInst()) {
-            res = res || my.DefaultAppName;
-        }
-        return res;
-    };
-
-    self.AllowDefaultEnvInst = function(allowdefaultenvinst_) {
-        var res = my.AllowDefaultEnvInst;
-        if (arguments.length > 0) {
-            if (allowdefaultenvinst_ !== null) {
-                LogProvider.ValidateAllowDefaultEnvInst(allowdefaultenvinst_, true);
-            }
-            my.AllowDefaultEnvInst = allowdefaultenvinst_;
-            res = self;
-        }
-        return res;
-    };
-
-    self.AllowDefaultAppName = function(allowdefaultappname_) {
-        var res = my.AllowDefaultAppName;
-        if (arguments.length > 0) {
-            if (allowdefaultappname_ !== null) {
-                LogProvider.ValidateAllowDefaultAppName(allowdefaultappname_, true);
-            }
-            my.AllowDefaultAppName = allowdefaultappname_;
-            res = self;
-        }
-        return res;
-    };
+  self.AllowDefaultAppName = function(allowdefaultappname_) {
+    var res = my.AllowDefaultAppName;
+    if (arguments.length > 0) {
+      if (allowdefaultappname_ !== null) {
+        LogProvider.ValidateAllowDefaultAppName(allowdefaultappname_, true);
+      }
+      my.AllowDefaultAppName = allowdefaultappname_;
+      res = self;
+    }
+    return res;
+  };
 }
 
 
@@ -108,55 +87,55 @@ function LogProvider() {
 //------------//
 
 LogProvider.ValidateEnvInst = function ValidateEnvInst(input, throwErr) {
-    var msg = '';
-    if (!Utils.instance_of(input, Environment)) {
-        msg = 'Invalid Argument: <LogProvider>.ValidateEnvInst requires an instance_of Environment argument';
-    }
+  var msg = '';
+  if (!Utils.instance_of(input, Environment)) {
+    msg = 'Invalid Argument: <LogProvider>.ValidateEnvInst requires an instance_of Environment argument';
+  }
 
-    if (throwErr && msg) {
-        throw new Error(msg);
-    }
+  if (throwErr && msg) {
+    throw new Error(msg);
+  }
 
-    return msg;
+  return msg;
 };
 
 LogProvider.ValidateAppName = function ValidateAppName(input, throwErr) {
-    var msg = '';
-    if (typeof input !== 'string') {
-        msg = 'Invalid Argument: <LogProvider>.ValidateAppName requires a typeof string argument';
-    }
+  var msg = '';
+  if (typeof input !== 'string') {
+    msg = 'Invalid Argument: <LogProvider>.ValidateAppName requires a typeof string argument';
+  }
 
-    if (throwErr && msg) {
-        throw new Error(msg);
-    }
+  if (throwErr && msg) {
+    throw new Error(msg);
+  }
 
-    return msg;
+  return msg;
 };
 
 LogProvider.ValidateAllowDefaultEnvInst = function ValidateAllowDefaultEnvInst(input, throwErr) {
-    var msg = '';
-    if (typeof input !== 'boolean') {
-        msg = 'Invalid Argument: <LogProvider>.ValidateDefaultEnvInst requires a typeof boolean argument';
-    }
+  var msg = '';
+  if (typeof input !== 'boolean') {
+    msg = 'Invalid Argument: <LogProvider>.ValidateDefaultEnvInst requires a typeof boolean argument';
+  }
 
-    if (throwErr && msg) {
-        throw new Error(msg);
-    }
+  if (throwErr && msg) {
+    throw new Error(msg);
+  }
 
-    return msg;
+  return msg;
 };
 
 LogProvider.ValidateAllowDefaultAppName = function ValidateAllowDefaultAppName(input, throwErr) {
-    var msg = '';
-    if (typeof input !== 'boolean') {
-        msg = 'Invalid Argument: <LogProvider>.ValidateDefaultAppName requires a typeof boolean argument';
-    }
+  var msg = '';
+  if (typeof input !== 'boolean') {
+    msg = 'Invalid Argument: <LogProvider>.ValidateDefaultAppName requires a typeof boolean argument';
+  }
 
-    if (throwErr && msg) {
-        throw new Error(msg);
-    }
+  if (throwErr && msg) {
+    throw new Error(msg);
+  }
 
-    return msg;
+  return msg;
 };
 
 
@@ -165,20 +144,20 @@ LogProvider.ValidateAllowDefaultAppName = function ValidateAllowDefaultAppName(i
 //-----------------------//
 
 LogProvider.prototype.getLogger = function getLogger() {
-    if (this.EnvInst() === null || this.AppName() === null) {
-        throw new Error("Invalid State: <LogProvider>.getLogger requires both EnvInst and AppName to either be set or allowed to use their defaults");
-    }
+  if (this.EnvInst() === null || this.AppName() === null) {
+    throw new Error("Invalid State: <LogProvider>.getLogger requires both EnvInst and AppName to either be set or allowed to use their defaults");
+  }
 
-    var bstream = bunyanStreams(this.AppName(), this.EnvInst().curEnv());
-    return bunyan.createLogger({
-        name: this.AppName()
-        , src: bstream.source
-        , streams: [{
-            level: bstream.level
-            , stream: bstream.stream
-            , type: bstream.type
-        }]
-    });
+  var bstream = bunyanStreams(this.AppName(), this.EnvInst().curEnv());
+  return bunyan.createLogger({
+    name: this.AppName()
+    , src: bstream.source
+    , streams: [{
+      level: bstream.level
+      , stream: bstream.stream
+      , type: bstream.type
+    }]
+  });
 };
 
 
